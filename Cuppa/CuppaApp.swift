0acfe229e94c54c84e02ce2b5cd2788cace34f05
Wasif Kamruddin <kamruddinwasif@gmail.com>
2023-04-12 23:46:33 +1000

    Initial Commit

---
+++ Cuppa/CuppaApp.swift
@@ -0,0 +1,17 @@
+//
+//  CuppaApp.swift
+//  Cuppa
+//
+//  Created by Wasif Kamruddin on 4/12/23.
+//
+
+import SwiftUI
+
+@main
+struct CuppaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
